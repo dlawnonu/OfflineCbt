e3ce06551f6a7236e044f4debf8207ee052a47f5
dlawnonu <ahmedajala92@yahoo.com>
2024-08-29 18:18:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Hello from './Hello'; // Import the Hello component
+
+const App = () => {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/Hello.jsx
@@ -0,0 +1,14 @@
+// src/Hello.jsx
+import React from 'react';
+import './Hello.css'; // Import the CSS file
+
+const Hello = () => {
+  return (
+    <div className="hello-container">
+      <h1 className="hello-heading">Welcome to the futur!</h1>
+    </div>
+  );
+};
+
+export default Hello;
+
